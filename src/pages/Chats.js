338777f23fb19b79236f0c88756880885edf125c
timfi <timfinmans20@gmail.com>
2022-05-17 19:38:16 +0200

    Specific chat view was optimized.

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -1,67 +1,84 @@
 import React from "react";
+import Data from "bootstrap/js/src/dom/data";
 
 function CreateChat() {
     return (
-        <div id="specific-chat" className="chat-box">
-            <img className="round-image-for-specific-chat" src="https://i.pravatar.cc/200"></img>
-            <p className="person-name">Name of the person</p>
-            <div className="line"></div>
-            <div className="placeholder-chat"></div>
-            <button onClick={()=>receiverMessage("Message is much muhc longer")}>Receiver</button>
-            <button onClick={()=>senderMessage("Message is much muhc longer")}>Sender</button>
+        <div>
+            <div id="specific-chat" className="chat-box">
+                <div className="floating-heading">
+                    <div className="chat-heading">
+                        <div className="image-holder">
+                            <img className="round-image-for-specific-chat" src="https://i.pravatar.cc/200"></img>
+                        </div>
+                        <p className="person-name">Username</p>
+                        <button onClick={()=>receiverMessage("Message is much muhc longer")}>Receiver</button>
+                        <button onClick={()=>senderMessage("Message is much  longer")}>Sender</button>
+                    </div>
+                </div>
+                <div className="message">
+                    <p className="message-text">
+                        Das ist eine sehr lange Nachricht mit einem Zeitstempel drunte.
+                    </p>
+                    <p className="timestemp-chat">10:45</p>
+                </div>
+            </div>
+            <textarea name="textarea" row="5" className="message-input" id="messageInput"/>
+            <button className="send-msg-btn-chat" onClick={()=>senderMessage()}>
+                <span className="material-icons">forum</span>
+            </button>
         </div>
     );
 }
 
 function receiverMessage(messageText) {
+
     const tag = document.createElement("div");
-    tag.className = "receiver";
+    tag.className = "message";
 
     const message = document.createElement("p");
     message.textContent = messageText;
-    message.className = "user-name";
+    message.className = "message-text";
+
+    const messageTimestemp = document.createElement("p");
+    messageTimestemp.textContent = "10:25";
+    messageTimestemp.className = "timestemp-chat";
 
     const element = document.getElementById("specific-chat");
     tag.appendChild(message);
+    tag.appendChild(messageTimestemp)
     element.appendChild(tag);
+
+    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "auto" });
 }
 
-
-function senderMessage(messageText) {
+function senderMessage(messageText=null) {
     const tag = document.createElement("div");
-    tag.className = "sender";
+    tag.className = "message sender";
+
+    const input = document.getElementById("messageInput");
 
     const message = document.createElement("p");
-    message.textContent = messageText;
-    message.className = "user-name";
+    if(messageText==null) {
+        if(input.value.isEmpty) {
+            return;
+        }
+        message.textContent = input.value;
+        input.value = "";
+    } else {
+        message.textContent = messageText;
+    }
+    message.className = "message-text";
+
+    const messageTimestemp = document.createElement("p");
+    messageTimestemp.textContent = "10:25";
+    messageTimestemp.className = "timestemp-chat";
 
     const element = document.getElementById("specific-chat");
     tag.appendChild(message);
+    tag.appendChild(messageTimestemp)
     element.appendChild(tag);
+
+    window.scrollTo(0, document.body.scrollHeight);
 }
-
-class Chats extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-
-    render() {
-        return (
-            <div id="chat" className="content-box home">
-                <h1>Chats</h1>
-                <button onClick={()=>this.addChat("Tim","Hi","10:57", "10.05.2001")}>Color</button>
-                <div className="content-box-in-box">
-                    <img className="round-image" src="https://i.pravatar.cc/200"></img>
-                    <p className="user-name">Username</p>
-                    <p className="last-message">Am Samstag habe ich leider keine Zeit...</p>
-                    <p className="time-date">10:47/</p>
-                    <p className="time-date">15.10.2001</p>
-                </div>
-            </div>
-        );
-    }
-}
-
 
 export default CreateChat;
