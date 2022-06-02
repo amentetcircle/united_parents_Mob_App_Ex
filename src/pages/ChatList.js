63820eb91fcd6c8d324ad5dd698032fd50c8f838
timfi <timfinmans20@gmail.com>
2022-06-02 21:20:32 +0200

    Changed the whole chat to a new better looking version like Whatsapp Web.

--- src/pages/ChatList.js
+++ src/pages/ChatList.js
@@ -1,169 +1,172 @@
 import React from "react";
 import {Link, Route, useNavigate} from 'react-router-dom'
+import context from "react-bootstrap/NavbarContext";
+import {forEach} from "react-bootstrap/ElementChildren";
 
 //Tim Finmans
-function Chat (){
-    let navigate = useNavigate();
+function start (){
 
-    // Hier müssen später mal die
-    let messages = [new ChatOverview("Lisa", "Ich kann nicht...",  "10:44", "16.04.1999"),
-        new ChatOverview("Moritz", "Komme gleich.",  "11:39", "7.04.1999"),
-        new ChatOverview("Martin", "Wie gehts?",  "12:54", "6.04.1999"),
-        new ChatOverview("Mira", "Na hör mal.",  "15:12", "5.04.1999"),
-        new ChatOverview("Faru", "Was soll das?",  "1:43", "4.04.1999")]
-
+    // will be called when the chat area is entered
     return(
-        <div id="chat" className="content-box">
-            <h1>Chats</h1>
-            <div className="content-box-in-box" onClick={()=>navigate("/specific-chat")}>
-                <img className="round-image" src="https://i.pravatar.cc/200"></img>
-                <p className="user-name">Username</p>
-                <p className="last-message">Am Samstag habe ich leider keine Zeit...</p>
-                <p className="time-date">10:47/</p>
-                <p className="time-date">15.10.2001</p>
+        <div id="whole-chat-view" className="whole-chat">
+            <div className="chat-list-box">
+                {newChats.map(chat=>{
+                    return(
+                        chat.addToViewOverview()
+                    )
+                })}
             </div>
+            <div id="chat-box" className="specific-chat">
+                <div id="to-remove-and-add"></div>
+            </div>
+            <textarea className="input">
 
-            {messages.map(messages=>{
-                return(
-                    <div className="content-box-in-box" onClick={()=>navigate("/specific-chat")}>
-                        <img className="round-image" src="https://i.pravatar.cc/200"></img>
-                        <p className="user-name">{messages.name}</p>
-                        <p className="last-message">{messages.lastMessage}</p>
-                        <p className="time-date">{messages.lastTimestemp}/</p>
-                        <p className="time-date">{messages.lastDate}</p>
-                    </div>
-                )
-            })}
-
+            </textarea>
         </div>
     );
-
 }
 
 //Tim Finmans
-class ChatOverview {
+class Chat {
     name;
-    lastMessage;
-    lastTimestemp;
+    lastMessage;    // will be unnecessary in the future because it can be gained by taking the arrays last element
+    lastTimestamp;
     lastDate;
+    messages = [];
+    specificChatHTML;
 
-    constructor(name, lastMessage, lastTimestemp, lastDate) {
+    // constructor to fill the variables
+    constructor(name, lastMessage, lastTimestemp, lastDate, messages) {
         this.name = name;
         this.lastMessage = lastMessage;
-        this.lastTimestemp = lastTimestemp;
+        this.lastTimestamp = lastTimestemp;
         this.lastDate = lastDate;
-    }
-}
-
-
-/*
-function AddChatOption(usernameT, lastMessageText, lastMessageTime, lastMessageDate) {
-    let navigate = useNavigate();
-    const element = document.getElementById("chat");
-    element.innerHTML+="<div>Hello</div>"
-}
-
-function addChat(usernameT, lastMessageText, lastMessageTime, lastMessageDate) {
-    const tag = document.createElement("div");
-    tag.className = "content-box-in-box";
-
-    const image = document.createElement("img");
-    image.src = "https://i.pravatar.cc/200";
-    image.className = "round-image";
-
-    const username = document.createElement("p");
-    username.textContent = usernameT;
-    username.className = "user-name";
-
-    const lastMessage = document.createElement("p");
-    lastMessage.textContent = lastMessageText;
-    lastMessage.className = "last-message";
-
-    const time = document.createElement("p");
-    time.textContent = lastMessageTime + "/";
-    time.className = "time-date";
-
-    const date = document.createElement("p");
-    date.textContent = lastMessageDate;
-    date.className = "time-date";
-
-    const element = document.getElementById("chat");
-    tag.appendChild(image);
-    tag.appendChild(username);
-    tag.appendChild(lastMessage);
-    tag.appendChild(time);
-    tag.appendChild(date);
-    tag.onclick()
-    element.appendChild(tag);
-}
-
-/*
-function Navigate() {
-    let navigate = useNavigate();
-    navigate("/specific-chat");
-}
-
-class ChatsList extends React.Component {
-    constructor(props) {
-        super(props);
+        messages.map(message=>{
+            this.messages.push(message)
+            }
+        )
     }
 
-    render() {
+    // add the chats that were already opened and can be chosen from the array
+    addToViewOverview(){
         return (
-            <div id="chat" className="content-box home">
-                <h1>Chats</h1>
-                <button onClick={()=>this.addChat("Tim","Hi","10:57", "10.05.2001")}>Color</button>
-                <button onClick={()=>Navigate("/specific-chat")}>Switch</button>
-
-                <div className="content-box-in-box">
-                    <img className="round-image" src="https://i.pravatar.cc/200"></img>
-                    <p className="user-name">Username</p>
-                    <p className="last-message">Am Samstag habe ich leider keine Zeit...</p>
-                    <p className="time-date">10:47/</p>
-                    <p className="time-date">15.10.2001</p>
-                    <Link to={"/specific-chat"}>Link</Link>
+            <div className="chat-overview" onClick={()=>this.renderChat()}>
+                <img className="round-image" src="https://i.pravatar.cc/200"></img>
+                <div className="text-wrapper-overview">
+                    <p className="user-name">{this.name}</p>
+                    <p className="last-message">{this.lastMessage}</p>
+                    <p className="timestamp-overview">{this.lastDate}</p>
                 </div>
             </div>
         );
     }
 
-    addChat(usernameT, lastMessageText, lastMessageTime, lastMessageDate) {
-        const tag = document.createElement("div");
-        tag.className = "content-box-in-box";
+    // rende a specific chat when a chat was clicked
+    renderChat(){
+        //delete the chat that is displayed at the moment
+        const deleteChatWindow = document.getElementById("to-remove-and-add");
+        deleteChatWindow.remove();
 
-        const image = document.createElement("img");
-        image.src = "https://i.pravatar.cc/200";
-        image.className = "round-image";
+        // create it again, set it to the same id
+        const chatWindow = document.createElement("div");
+        chatWindow.id = "to-remove-and-add";
+        chatWindow.className = "to-remove-and-add"
 
-        const username = document.createElement("p");
-        username.textContent = usernameT;
-        username.className = "user-name";
+        // loop through the messages and add them to the overview
+        this.messages.map(message=>{
+            const image = document.createElement("img")
+            const containerForMessage = document.createElement("div");
+            if(message.type === "r"){
+                containerForMessage.className = "message";
+                image.className = "round-image-for-chat"
+                image.src = "https://i.pravatar.cc/200"
+            } else if(message.type === "s") {
+                containerForMessage.className = "message sender";
+                image.className = "round-image-for-chat receiver-image"
+                image.src = "https://i.pravatar.cc/300"
+            }
 
-        const lastMessage = document.createElement("p");
-        lastMessage.textContent = lastMessageText;
-        lastMessage.className = "last-message";
+            const text = document.createElement("p");
+            text.textContent = message.text;
+            text.className = "message-text";
 
-        const time = document.createElement("p");
-        time.textContent = lastMessageTime + "/";
-        time.className = "time-date";
+            const messageTimestamp = document.createElement("p");
+            messageTimestamp.textContent = message.timestamp;
+            messageTimestamp.className = "timestamp-chat";
 
-        const date = document.createElement("p");
-        date.textContent = lastMessageDate;
-        date.className = "time-date";
+            containerForMessage.appendChild(text);
+            containerForMessage.appendChild(messageTimestamp)
+            chatWindow.appendChild(image)
+            chatWindow.appendChild(containerForMessage);
+        })
 
-        const element = document.getElementById("chat");
-        tag.appendChild(image);
-        tag.appendChild(username);
-        tag.appendChild(lastMessage);
-        tag.appendChild(time);
-        tag.appendChild(date);
-        tag.addEventListener("click", test);
-        element.appendChild(tag);
 
-        function test() {
-            lastMessage.textContent = "Ja"
+        const wholeChatWindow = document.getElementById("chat-box");
+        wholeChatWindow.appendChild(chatWindow);
+        wholeChatWindow.scrollTop = wholeChatWindow.scrollHeight;
+    }
+}
+
+class Message {
+    text;
+    timestamp;
+    type;   // r = received, s = send
+
+    constructor(text, timestamp, type) {
+        this.text = text;
+        this.timestamp = timestamp;
+        this.type = type;
+    }
+
+    addToView() {
+        if(this.type === 's'){
+            return(
+                <div className="message sender">
+                    <p className="message-text">
+                        {this.text}
+                    </p>
+                    <p className="timestamp-chat">
+                        {this.timestamp}
+                    </p>
+                </div>
+            )
+        } else if(this.type === 'r') {
+            return (
+                <div className="message">
+                    <p className="message-text">
+                        {this.text}
+                    </p>
+                    <p className="timestamp-chat">
+                        {this.timestamp}
+                    </p>
+                </div>
+            );
         }
     }
 }
-*/
-export default Chat;
+
+var newMessages = [new Message("Na, wie geht es dir?", "10:40", "s"),
+    new Message("Mir geht es super, danke der Nachfrage.", "10:41", "r"),
+    new Message(":D", "10:44", "s"),
+    new Message("Und dir?", "10:47", "r"),
+    new Message("Hey", "10:40", "s"),
+    new Message("Was macht die Kunst bei dir heute so an einem schönen Samstag?.", "10:41", "r"),
+    new Message("Fußball schauen und Bierchen trinken.", "10:44", "s"),
+    new Message("Na das wollen wir ja mal sehen was da bei raus kommt.", "10:47", "s")]
+
+var newMessages2 = [new Message("Hey", "10:40", "s"),
+    new Message("Was macht die Kunst bei dir heute so an einem schönen Samstag?.", "10:41", "r"),
+    new Message("Fußball schauen und Bierchen trinken.", "10:44", "s"),
+    new Message("Na das wollen wir ja mal sehen was da bei raus kommt.", "10:47", "s")]
+
+var newChats = [new Chat("Tim","Hi wie gehts?","10.07.2001","11:25", newMessages),
+    new Chat("Mennwin","Hi wie gehts dir und deiner ganze Familie?","10.07.2001","11:25", newMessages2),
+    new Chat("Max","Drei Mal darfst du raten :)","10.07.2001","11:25", newMessages),
+    new Chat("Katharina","Übermorgen leider erst wieder","10.07.2001","11:25", newMessages),
+    new Chat("Martina","Hi wie gehts?","10.07.2001","11:25", newMessages),
+    new Chat("Thomas","Naja, das muss jetzt nicht zwingend sein aber wenn du willst.","10.07.2001","11:25", newMessages),
+    new Chat("Andreas","Hey","10.07.2001","11:25", newMessages),
+    new Chat("Herr Winkelmann","Völlig Normal!","10.07.2001","11:25", newMessages),
+    new Chat("Jason","Das kann ja wohl nicht sein?!","10.07.2001","11:25", newMessages)];
+
+export default start;
